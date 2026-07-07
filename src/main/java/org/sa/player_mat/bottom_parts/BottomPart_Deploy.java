ba9ff30dc952ceccfded8994327d807e20546fc0
Simas <simonas.aruna@gmail.com>
2026-07-07 11:37:47 +0300

    master: create BottomPartUpgradableCost__Metal (for bottom part deploy)

---
+++ src/main/java/org/sa/player_mat/bottom_parts/BottomPart_Deploy.java
@@ -0,0 +1,66 @@
+package org.sa.player_mat.bottom_parts;
+
+import org.sa.enums.ResourceType;
+import org.sa.player_mat.PlayerMatType;
+import org.sa.player_mat.bottom_parts.bottom_part_coin_benefit.BottomPartCoinBenefit;
+import org.sa.player_mat.bottom_parts.bottom_part_cost.BottomPartUpgradableCost;
+import org.sa.player_mat.bottom_parts.bottom_part_cost.BottomPartUpgradableCost__Metal;
+import org.sa.player_mat.bottom_parts.enums_and_interfaces.BottomPart;
+import org.sa.player_mat.bottom_parts.enums_and_interfaces.BottomPartType;
+
+public class BottomPart_Deploy implements BottomPart {
+
+  // types
+  public PlayerMatType playerMatType;  //example AGRICULTURAL
+  public BottomPartType BOTTOM_PART_TYPE__DEPLOY = BottomPartType.DEPLOY;
+
+  // state changes
+  public BottomPartUpgradableCost__Metal bottomPartUpgradableCost__metal;
+  public BottomPartCoinBenefit bottomPartCoinBenefit;
+
+  // neighbor bonus
+  public BottomPartType BOTTOM_PART_NEIGHBOR_TRIGGER__DEPLOY = BottomPartType.DEPLOY; // what neighbour bottom action triggers neighbor benefit
+  public ResourceType BOTTOM_PART_NEIGHBOR_BENEFIT_TYPE__ATTACK = ResourceType.ATTACK;  //common for all player mat DEPLOY actions
+  public int BOTTOM_PART_NEIGHBOR_BENEFIT_DELTA__ONE = 1; //absolutely common in all cases (all player mats, all actions)
+
+  /** constructor */
+  public BottomPart_Deploy(PlayerMatType playerMatType) {
+    this.playerMatType = playerMatType;
+    this.bottomPartCoinBenefit = new BottomPartCoinBenefit(this.BOTTOM_PART_TYPE__DEPLOY, playerMatType);
+    this.bottomPartUpgradableCost__metal = new BottomPartUpgradableCost__Metal(playerMatType);
+  }
+
+  // types
+  @Override
+  public PlayerMatType getPlayerMatType() {
+    return this.playerMatType;
+  }
+  @Override
+  public BottomPartType getBottomPartType() {
+    return this.BOTTOM_PART_TYPE__DEPLOY;
+  }
+
+  // state changes
+  @Override
+  public BottomPartUpgradableCost getBottomPart_UpgradableCost() {
+    return this.bottomPartUpgradableCost__metal;
+  }
+  @Override
+  public BottomPartCoinBenefit getBottomPartCoinBenefit() {
+    return this.bottomPartCoinBenefit;
+  }
+
+  // neighbor bonus
+  @Override
+  public BottomPartType getType_NeighborTrigger() {
+    return this.BOTTOM_PART_NEIGHBOR_TRIGGER__DEPLOY;
+  }
+  @Override
+  public ResourceType getType_NeighborBenefit() {
+    return this.BOTTOM_PART_NEIGHBOR_BENEFIT_TYPE__ATTACK;
+  }
+  @Override
+  public int getDelta_NeighborBenefit_ONE() {
+    return this.BOTTOM_PART_NEIGHBOR_BENEFIT_DELTA__ONE;
+  }
+}
---
+++ src/main/java/org/sa/player_mat/bottom_parts/bottom_part_cost/BottomPartUpgradableCost__Metal.java
@@ -0,0 +1,43 @@
+package org.sa.player_mat.bottom_parts.bottom_part_cost;
+
+import org.sa.DTO.PlayerDTO;
+import org.sa.enums.ResourceType;
+import org.sa.player_mat.PlayerMatType;
+
+public class BottomPartUpgradableCost__Metal implements BottomPartUpgradableCost {
+  private int currentDeltaMetal;
+  private int fullyUpgradedDeltaMetalCost;
+
+  public BottomPartUpgradableCost__Metal(PlayerMatType playerMatType) {
+    this.currentDeltaMetal = switch (playerMatType) {
+      case AGRICULTURAL, INNOVATIVE, ENGINEERING, MILITANT  -> -2;
+      case MECHANICAL, INDUSTRIAL, PATRIOTIC -> -1;
+    };
+
+    this.fullyUpgradedDeltaMetalCost = switch (playerMatType) {
+      case AGRICULTURAL, ENGINEERING, PATRIOTIC -> -4;
+      case INDUSTRIAL, INNOVATIVE, MECHANICAL, MILITANT -> -3;
+    };
+  }
+
+  @Override
+  public void applyToPlayer(PlayerDTO player) {
+    if (currentDeltaMetal > 0) throw new IllegalArgumentException("Cost should be expressed in negative numbers");
+    player.getResourceMap().merge(ResourceType.METAL, currentDeltaMetal, Integer::sum); //because negative numbers
+  }
+
+  @Override
+  public int getCurrentChangeDelta() {
+    return this.currentDeltaMetal;
+  }
+
+  @Override
+  public boolean canUpgrade() {
+    return currentDeltaMetal < fullyUpgradedDeltaMetalCost; //because negative numbers
+  }
+
+  @Override
+  public void upgrade_lowerTheCost() {
+    currentDeltaMetal++; //because negative numbers
+  }
+}
