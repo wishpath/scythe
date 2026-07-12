fcd4a58e8d95485c3c2532bcd283db5be177d34d
Simas <simonas.aruna@gmail.com>
2026-07-12 17:08:21 +0300

    master: fix the price of oil

--- src/main/java/org/sa/player_mat/bottom_parts/bottom_part_cost/BottomPartUpgradableCost__Oil.java
+++ src/main/java/org/sa/player_mat/bottom_parts/bottom_part_cost/BottomPartUpgradableCost__Oil.java
@@ -14,17 +14,22 @@ public class BottomPartUpgradableCost__Oil implements BottomPartUpgradableCost {
       case AGRICULTURAL, PATRIOTIC -> -2;
       case ENGINEERING, INNOVATIVE, INDUSTRIAL, MECHANICAL, MILITANT -> -3;
     };
+
     this.fullyUpgradedDeltaOilCost = switch (playerMatType) {
       case AGRICULTURAL, ENGINEERING, INDUSTRIAL, MECHANICAL, PATRIOTIC -> -2;
       case INNOVATIVE -> -3;
       case MILITANT -> -1;
     };
+
+    if (this.currentDeltaOil > this.fullyUpgradedDeltaOilCost) { //because expressed in negative numbers
+      throw new IllegalArgumentException("Upgraded price cannot be higher");
+    }
   }
 
   @Override
   public void applyToPlayer(PlayerDTO player) {
     if (currentDeltaOil > 0) throw new IllegalArgumentException("Cost should be expressed in negative numbers");
-    player.getResourceMap().merge(ResourceType.OIL, currentDeltaOil, Integer::sum); //because negative numbers
+    player.addResource(ResourceType.OIL, currentDeltaOil);
   }
 
   @Override
@@ -40,5 +45,9 @@ public class BottomPartUpgradableCost__Oil implements BottomPartUpgradableCost {
   @Override
   public void upgrade_lowerTheCost() {
     currentDeltaOil++; //because negative numbers
+
+    if (this.currentDeltaOil > this.fullyUpgradedDeltaOilCost) { //because expressed in negative numbers
+      throw new IllegalArgumentException("Upgraded price cannot be higher");
+    }
   }
 }
