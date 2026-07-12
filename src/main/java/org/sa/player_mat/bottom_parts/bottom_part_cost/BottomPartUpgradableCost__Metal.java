913ed47114fc47dbc9456087cd3f56401439c258
Simas <simonas.aruna@gmail.com>
2026-07-12 17:05:32 +0300

    master: fix cost of metal

--- src/main/java/org/sa/player_mat/bottom_parts/bottom_part_cost/BottomPartUpgradableCost__Metal.java
+++ src/main/java/org/sa/player_mat/bottom_parts/bottom_part_cost/BottomPartUpgradableCost__Metal.java
@@ -11,20 +11,24 @@ public class BottomPartUpgradableCost__Metal implements BottomPartUpgradableCost
 
   public BottomPartUpgradableCost__Metal(PlayerMatType playerMatType) {
     this.currentDeltaMetal = switch (playerMatType) {
+      case AGRICULTURAL, ENGINEERING, PATRIOTIC -> -4;
+      case INDUSTRIAL, INNOVATIVE, MECHANICAL, MILITANT -> -3;
+    };
+
+    this.fullyUpgradedDeltaMetalCost = switch (playerMatType) {
       case AGRICULTURAL, INNOVATIVE, ENGINEERING, MILITANT  -> -2;
       case MECHANICAL, INDUSTRIAL, PATRIOTIC -> -1;
     };
 
-    this.fullyUpgradedDeltaMetalCost = switch (playerMatType) {
-      case AGRICULTURAL, ENGINEERING, PATRIOTIC -> -4;
-      case INDUSTRIAL, INNOVATIVE, MECHANICAL, MILITANT -> -3;
-    };
+    if (this.currentDeltaMetal > this.fullyUpgradedDeltaMetalCost) { //because expressed in negative numbers
+      throw new IllegalArgumentException("Upgraded price cannot be higher");
+    }
   }
 
   @Override
   public void applyToPlayer(PlayerDTO player) {
     if (currentDeltaMetal > 0) throw new IllegalArgumentException("Cost should be expressed in negative numbers");
-    player.getResourceMap().merge(ResourceType.METAL, currentDeltaMetal, Integer::sum); //because negative numbers
+    player.addResource(ResourceType.METAL, currentDeltaMetal); //currentDeltaMetal is a negative number
   }
 
   @Override
@@ -40,5 +44,8 @@ public class BottomPartUpgradableCost__Metal implements BottomPartUpgradableCost
   @Override
   public void upgrade_lowerTheCost() {
     currentDeltaMetal++; //because negative numbers
+    if (this.currentDeltaMetal > this.fullyUpgradedDeltaMetalCost) { //because expressed in negative numbers
+      throw new IllegalArgumentException("Upgraded price cannot be higher");
+    }
   }
 }
