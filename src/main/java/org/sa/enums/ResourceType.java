dd286523a0d50c3af00c19765a9bcd8125e89b64
Simas <simonas.aruna@gmail.com>
2026-08-12 10:58:11 +0300

    master: add max resource ammounts; add board picture; notes

--- src/main/java/org/sa/enums/ResourceType.java
+++ src/main/java/org/sa/enums/ResourceType.java
@@ -1,25 +1,27 @@
 package org.sa.enums;
 
 public enum ResourceType {
-  FOOD(true, true),
-  METAL(true, true),
-  OIL(true, true),
-  WOOD(true, true),
+  FOOD(true, true, null),
+  METAL(true, true, null),
+  OIL(true, true, null),
+  WOOD(true, true, null),
 
-  COINS(false, true),
-  HEARTS(false, true),
-  ATTACK(false, true),
+  COINS(false, true, null),
+  HEARTS(false, true, 18),
+  ATTACK(false, true, 16),
 
-  COMBAT_CARDS(false, false),
-  MISSION_CARDS(false, false),
+  COMBAT_CARDS(false, false, null),
+  MISSION_CARDS(false, false, null),
 
-  WORKERS(false, false);
+  WORKERS(false, false, 8);
 
   public final boolean canGetThroughTradeAction;
   public final boolean isIntegerResource;
+  public final Integer maxAmountPlayerCanHave;
 
-  ResourceType(boolean canGetThroughTradeAction, boolean isIntegerResource) {
+  ResourceType(boolean canGetThroughTradeAction, boolean isIntegerResource, Integer maxAmountPlayerCanHave) {
     this.canGetThroughTradeAction = canGetThroughTradeAction;
     this.isIntegerResource = isIntegerResource;
+    this.maxAmountPlayerCanHave = maxAmountPlayerCanHave;
   }
 }
