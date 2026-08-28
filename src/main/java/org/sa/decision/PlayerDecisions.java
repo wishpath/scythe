1905f7929cac5cc7ded51e0988a4505616b9a22b
Simas <simonas.aruna@gmail.com>
2026-08-28 23:18:17 +0300

    master: gain combat cards from armory; notes

--- src/main/java/org/sa/decision/PlayerDecisions.java
+++ src/main/java/org/sa/decision/PlayerDecisions.java
@@ -198,12 +198,19 @@ public class PlayerDecisions {
   }
 
   private static void DECIDE_andApply_TopAction_TRADE(TopPartUpgradableAction_Trade_Decideable tradeAction, PlayerDTO player) {
+    //choose resource types
     LocatableResourceType tradeableResourceType1 = LocatableResourceType.getTradeableResourceTypes().get(0); //TODO: player picks resource type1 to get
     LocatableResourceType tradeableResourceType2 = LocatableResourceType.getTradeableResourceTypes().get(0); //TODO: player picks resource type2 to get
     if (tradeableResourceType1 == null || tradeableResourceType2 == null || !tradeableResourceType1.canGetThroughTradeAction || !tradeableResourceType2.canGetThroughTradeAction) throw new IllegalArgumentException("plyer should pick a tradeable resource");
+
+    // place resources on the board
     TileDTO mostPowerfulTileWithWorkers = player.getWorkerTileWithMostPower(); //for the sake of simplicity defining best tile algorithmically. otherwise player could choose.
     player.addTradeableResource(tradeableResourceType1, 1, mostPowerfulTileWithWorkers);
     player.addTradeableResource(tradeableResourceType2, 1, mostPowerfulTileWithWorkers);
+
+    //gain combat cards from armory
+    if (player.isBuilt(BuildingType.ARMORY)) player.addCombatCard(1);
+
     //no after move effects??
     player.isRightAfterMove = true;
     player.isRightAfterMove = false;
